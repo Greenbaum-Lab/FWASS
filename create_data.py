47fa5fd73251c368b62519e27096e15a95cb3a96
shaharMazia <shahar.mazie@mail.huji.ac.il>
2023-07-07 16:41:06 +0300

    Start experiments with msprime

--- create_data.py
+++ create_data.py
@@ -5,9 +5,8 @@ def run_simulation(num_indv, num_snps):
     demography = msprime.Demography()
     demography.add_population(name='pop', initial_size=10000)
 
-    ts = msprime.sim_ancestry(samples={'a': num_indv},
-                              demography=demography,
-                              recombination_rate=1e-8, random_seed=1)
+    ts = msprime.sim_ancestry(samples={'a': num_indv}, demography=demography, recombination_rate=1e-8, random_seed=1,
+                              sequence_length=num_snps)
     mts = msprime.sim_mutations(ts, model=msprime.BinaryMutationModel(), rate=8e-7, random_seed=1)
     return mts
 
