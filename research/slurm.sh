86e2e87e79fc3944f4fdeb4b599371cee7dd5c94
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-05-31 16:11:39 +0300

    Comparison experiments

--- research/slurm.sh
+++ research/slurm.sh
@@ -3,8 +3,9 @@
 
 #SBATCH -t 3-00:00 # Runtime in D-HH:MM
 #SBATCH --cpus-per-task=1 # Request that ncpus be allocated per process.
-
+echo "Loading python"
 module load python
+echo "Loaded python"
 source /sci/labs/gilig/shahar.mazie/icore-data/snpnmi_venv/bin/activate
 
 echo "Start running jobs $comp"
