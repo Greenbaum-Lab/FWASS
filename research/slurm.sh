7317617fd05421c563c99770cfd7dbe5ff6ede72
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-06-03 17:05:03 +0300

    Fix compare between outputs

--- research/slurm.sh
+++ research/slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 3-00:00 # Runtime in D-HH:MM
 #SBATCH --cpus-per-task=1 # Request that ncpus be allocated per process.
 module load python
-echo "Start running jobs $comp to $outputs"
+echo "Start running jobs $comp to $output"
 mkdir $output
 mkdir $output/$comp/
 srun python research/compare_runner.py -o $output/$comp/asd --method asd --mock --comparison_name $comp
