d82b88bb9c7d2464b9763155a4a7705de1297dd2
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-06-03 15:43:32 +0300

    Comparison experiments

--- research/slurm.sh
+++ research/slurm.sh
@@ -4,10 +4,8 @@
 #SBATCH -t 3-00:00 # Runtime in D-HH:MM
 #SBATCH --cpus-per-task=1 # Request that ncpus be allocated per process.
 module load python
-source /sci/labs/gilig/shahar.mazie/icore-data/snpnmi_venv/bin/activate
-
 echo "Start running jobs $comp"
 mkdir /sci/labs/gilig/shahar.mazie/icore-data/FWASS_research/$comp/
-srun python /sci/labs/gilig/shahar.mazie/icore-data/code/FWASS/research/compare_runner.py -o /sci/labs/gilig/shahar.mazie/icore-data/FWASS_research/$comp/asd --method asd --mock --comparison_name $comp
-srun python /sci/labs/gilig/shahar.mazie/icore-data/code/FWASS/research/compare_runner.py -o /sci/labs/gilig/shahar.mazie/icore-data/FWASS_research/$comp/similarity --method similarity --mock --comparison_name $comp
-srun python /sci/labs/gilig/shahar.mazie/icore-data/code/FWASS/research/compare_runner.py -o /sci/labs/gilig/shahar.mazie/icore-data/FWASS_research/$comp/weighted_similarity --method similarity -w 1 --mock --comparison_name $comp
+srun python research/compare_runner.py -o $output/$comp/asd --method asd --mock --comparison_name $comp
+srun python research/compare_runner.py -o $output/$comp/similarity --method similarity --mock --comparison_name $comp
+srun python research/compare_runner.py -o $output/$comp/weighted_similarity --method similarity -w 1 --mock --comparison_name $comp
