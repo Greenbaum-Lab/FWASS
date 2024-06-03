157def0640acb1dddd937140c134388935f811d4
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-06-03 19:48:05 +0300

    Fix compare between outputs

--- research/slurm.sh
+++ research/slurm.sh
@@ -2,11 +2,12 @@
 #
 
 #SBATCH -t 3-00:00 # Runtime in D-HH:MM
-#SBATCH -n 3 # Request that n cpus
+#SBATCH --ntasks 3  # Request that n cpus
+#SBATCH --cpus-per-task 1  # Request that n cpus
 module load python
 echo "Start running jobs $comp to $output"
 mkdir -p $output/$comp/
-srun python research/compare_runner.py -o $output/$comp/asd --method asd --mock --comparison_name $comp &
-srun python research/compare_runner.py -o $output/$comp/similarity --method similarity --mock --comparison_name $comp &
-srun python research/compare_runner.py -o $output/$comp/weighted_similarity --method similarity -w 1 --mock --comparison_name $comp &
+srun python research/compare_runner.py --mock --comparison_name $comp -o $output/$comp/asd --method asd &
+srun python research/compare_runner.py --mock --comparison_name $comp -o $output/$comp/similarity --method similarity &
+srun python research/compare_runner.py --mock --comparison_name $comp -o $output/$comp/weighted_similarity --method similarity -w 1 &
 wait
