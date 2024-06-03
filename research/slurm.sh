02f1b99f0e73ae26d72bc6a16d5a4cc775c9abbe
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-06-03 16:36:44 +0300

    Comparison experiments

--- research/slurm.sh
+++ research/slurm.sh
@@ -4,8 +4,9 @@
 #SBATCH -t 3-00:00 # Runtime in D-HH:MM
 #SBATCH --cpus-per-task=1 # Request that ncpus be allocated per process.
 module load python
-echo "Start running jobs $comp"
-mkdir /sci/labs/gilig/shahar.mazie/icore-data/FWASS_research/$comp/
+echo "Start running jobs $comp to $outputs"
+mkdir $output
+mkdir $output/$comp/
 srun python research/compare_runner.py -o $output/$comp/asd --method asd --mock --comparison_name $comp
 srun python research/compare_runner.py -o $output/$comp/similarity --method similarity --mock --comparison_name $comp
 srun python research/compare_runner.py -o $output/$comp/weighted_similarity --method similarity -w 1 --mock --comparison_name $comp
