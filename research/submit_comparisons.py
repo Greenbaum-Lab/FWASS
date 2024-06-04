c5037421c0190c65ce9b12347bb38efbafd4cb6b
shaharMazia <shahar.mazie@mail.huji.ac.il>
2024-06-04 19:23:12 +0300

    try new submit_comparisons.py

--- research/submit_comparisons.py
+++ research/submit_comparisons.py
@@ -6,6 +6,7 @@ from research.compare_runner import experiment_argument_parser
 if __name__ == '__main__':
     arguments = experiment_argument_parser(check_method=False)
     submit_helper_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'slurm.sh')
+    wrapper_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'wrapper.sh')
     compare_runner_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'compare_runner.py')
     os.makedirs(os.path.join(arguments.output, 'logs'), exist_ok=True)
     for comp in ['num_of_individuals', 'num_of_snps']:
@@ -18,5 +19,5 @@ if __name__ == '__main__':
                       f"{os.path.join(arguments.output, comp, job_name)} --method {method}"
             if arguments.mock:
                 command += " --mock"
-            cmd2run = f"{sbatch_settings} {submit_helper_path} {command}"
+            cmd2run = f"{sbatch_settings} {wrapper_path} {command}"
             subprocess.run([submit_helper_path, cmd2run])
